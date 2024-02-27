17783467ff1d063f17c3410c3a4aabb9151590e9
jose <jose.minguez-garrigues@deutschebahn.com>
2024-02-27 12:10:15 +0100

    Update smart contract

--- contract/contract.sol
+++ contract/contract.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 contract ParkingLot {
     address public owner;
     mapping(address => uint256) public entryTimes;
-    uint256 public constant hourlyRate = 0.00001 ether; // Adjust as needed
+    mapping(address => uint256) public etherCredit;
+    uint256 public constant hourlyRate = 0.1 ether; // Adjust as needed
 
-    event CarEntered(address indexed car, uint256 entryTime);
+    event CarEntered(address indexed car, uint256 entryTime, uint256 ethCredit);
     event CarExited(address indexed car, uint256 exitTime, uint256 fee);
 
     constructor() {
@@ -20,14 +21,15 @@ contract ParkingLot {
     function enter() external payable {
         require(msg.value >= hourlyRate, "Insufficient payment");
         entryTimes[msg.sender] = block.timestamp;
-        emit CarEntered(msg.sender, block.timestamp);
+        etherCredit[msg.sender] = msg.value;
+        emit CarEntered(msg.sender, block.timestamp, msg.value);
     }
 
     function exit() external {
         require(entryTimes[msg.sender] > 0, "Car not registered");
-        uint256 parkedHours = (block.timestamp - entryTimes[msg.sender]) / 3600; // Calculating parked hours
+        uint256 parkedHours = (block.timestamp - entryTimes[msg.sender]); // Calculating parked seconds
         uint256 parkingFee = parkedHours * hourlyRate;
-        payable(msg.sender).transfer(parkingFee); // Refund any excess payment
+        payable(msg.sender).transfer(etherCredit[msg.sender] - parkingFee); // Refund any excess payment
         delete entryTimes[msg.sender];
         emit CarExited(msg.sender, block.timestamp, parkingFee);
     }
